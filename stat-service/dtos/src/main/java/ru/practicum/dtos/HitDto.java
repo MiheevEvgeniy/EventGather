d827b1c3e0424bac86d7e3fa85a1f81e1de6cd89
KlingTone <zhenya939393@yandex.ru>
2023-07-24 00:31:29 +0300

    кодстайл 2

--- stat-service/dtos/src/main/java/ru/practicum/dtos/HitDto.java
+++ stat-service/dtos/src/main/java/ru/practicum/dtos/HitDto.java
@@ -1,19 +1,10 @@
 package ru.practicum.dtos;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
-import java.time.LocalDateTime;
 
 @Data
 @Builder
